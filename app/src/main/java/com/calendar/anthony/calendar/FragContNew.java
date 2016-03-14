6eb6544d8d703de635c3cbb2ce226d8f5b7810d5
Anthony <aturbanek@wisc.edu>
2016-03-14 16:07:34 -0500

    Added buttons and activities for adding and viewing events.

---
+++ app/src/main/java/com/calendar/anthony/calendar/FragContNew.java
@@ -0,0 +1,13 @@
+package com.calendar.anthony.calendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragContNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frag_cont_new);
+    }
+}
---
+++ app/src/main/java/com/calendar/anthony/calendar/FragContView.java
@@ -0,0 +1,13 @@
+package com.calendar.anthony.calendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragContView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frag_cont_view);
+    }
+}
